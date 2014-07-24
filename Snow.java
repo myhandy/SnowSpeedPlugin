77952e10ea6bbf87f8226afba150e05c73267a82
myhandy <myhandy@users.noreply.github.com>
2014-07-24 01:18:42 -0600

    Update Snow.java

--- Snow.java
+++ Snow.java
@@ -1,27 +1,23 @@
 package com.myhandy.SnowSpeed;
 
-//import java.util.List;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 public final class Snow extends JavaPlugin {
-	public World WRLD = (World) getConfig().get("WORLD");
 	public void onEnable(){
+		System.out.println(Bukkit.getServer().getWorlds());
+		this.saveDefaultConfig();
 		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
 		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
 	   	  	@Override
 	   	  	public void run() {
-	   	  		System.out.println("step one works fine"); {
-	   	  			for(Player p : Bukkit.getOnlinePlayers()){
-	   	  				if(p.getWorld().equals(WRLD)) {
-	                	 System.out.println("World check is working for player: "+ p.getDisplayName());
-	                 	}
-	   	  				else{
-	   	  					continue;
-	   	  				}
+	   	  		for(Player p : Bukkit.getOnlinePlayers()){
+	   	  			if(p.getWorld().getName().equals(Snow.this.getConfig().getString("WORLD"))) {
+	                	 	System.out.println("World check is working for player: "+ p.getName());
+	                 		}else{
+	   	  			continue;
 	   	  			}
 	   	  		}
 	   	  	}
